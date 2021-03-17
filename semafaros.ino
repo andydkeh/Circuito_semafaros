d27040a587f8f829090f41ef06ee7bc960653e1f
andydkeh <endhygranville@gmail.com>
2021-03-16 22:21:32 -0300

    Primeiros passos

---
+++ semafaros.ino
@@ -0,0 +1,21 @@
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+digitalWrite(13, HIGH);
+    delay(1000);
+        digitalWrite(13, LOW);
+  
+digitalWrite(12, HIGH);
+    delay(1000); 
+        digitalWrite(12, LOW);
+
+digitalWrite(4, HIGH);
+    delay(1000); 
+        digitalWrite(4, LOW);  
+}
